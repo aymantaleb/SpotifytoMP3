bb4434122e44adee4276ef6fa25f36029fa78723
aymantaleb <ayman1779@gmail.com>
2023-08-24 03:27:50 -0700

    beginning GUI

---
+++ GUI.py
@@ -0,0 +1,16 @@
+import customtkinter
+from PIL import Image
+
+def button_callback():
+    print("button pressed")
+
+
+if __name__ == "__main__":
+    app = customtkinter.CTk()
+
+    app.title("my app")
+    app.geometry("400x400")
+    button_image = customtkinter.CTkImage(Image.open("AlbumArt/A.D.H.D.jpeg"), size=(160, 160))
+    button = customtkinter.CTkButton(master = app, text="my button", command=button_callback, image=button_image)
+    button.grid(row=0, column=0, padx=20, pady=20)
+    app.mainloop()
